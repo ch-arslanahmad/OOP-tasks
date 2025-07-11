0fde2c96d8507420edba510043c647a0a97c6d62
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-10 21:23:33 -0700

    feat: made variables and object & printed its first attribute

--- week#2/Product.java
+++ week#2/Product.java
@@ -1,3 +1,21 @@
 class Product {
-
+    String n;
+    String ID;
+    private float c;
+    private int quan;
+    float pri;
+    
+    Product (String name, String Product_ID, float cost, int quantity, float price) {
+        this.n = name;
+        this.ID = Product_ID;
+        this.c = cost;
+        this.quan = quantity;
+        this.pri = price;
+    }
+    public static void main(String[] args) {
+        Product Apple = new Product("Apple", "A0001", 1.5f, 100, 3);
+        
+     System.out.print(Apple.c);
+    }
+    
 }
