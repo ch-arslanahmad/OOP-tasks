9bfa688e009285acc98ffb75e2a030241d039a50
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-11 16:17:26 +0500

    docs: added initial comments

--- week#2/Product.java
+++ week#2/Product.java
@@ -1,10 +1,12 @@
 class Product {
+// instance variables 
     String n;
     String ID;
     private float c;
     private int quan;
     float pri;
-    
+
+// Constructor for object variables
     Product (String name, String Product_ID, float cost, int quantity, float price) {
         this.n = name;
         this.ID = Product_ID;
@@ -12,9 +14,10 @@ class Product {
         this.quan = quantity;
         this.pri = price;
     }
+    // main method
     public static void main(String[] args) {
         Product Apple = new Product("Apple", "A0001", 1.5f, 100, 3);
-        
+    // simply prints the variable of cost
      System.out.print(Apple.c);
     }
     
