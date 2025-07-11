c0b0269685f5ee793ad7f0d7e2f79f6d168a23a8
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-11 18:34:20 +0500

    Create Book.cpp

---
+++ week#3/Book.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+class Book {
+// private members
+private:
+// relevent instance variables for Book class
+    std::string bname;
+    std::string author;
+    float  author_commission;
+    int year_published;
+
+// public members
+public:
+// constructor
+    Book(std::string bname, std::string author, float author_commission, int year_published) : bname(bname), author(author), author_commission(author_commission), year_published(year_published) {}
+    
+    //  getter for name
+    std::string getbName() {
+        return bname;
+    }
+    //  getter for private author
+    std::string getAuthor() {
+        return author;
+    }
+    // setter for author to update the variable
+    void setAuthor(std::string new_Author) {
+        author = new_Author;
+    }
+    
+};
+
+int main() {
+// Making an object named: Book1
+        Book Book1("How to write in Java?", "Ali", 5.5f, 2000);
+
+// call a getter to print book name
+        std::cout<<"Book: "<<Book1.getbName();
+// call a getter to print author name
+        std::cout<<"\nAuthor: "<<Book1.getAuthor();
+// call the setter to update the author variable
+        Book1.setAuthor("Arslan");
+// now call the updated (done using the previous setter) author variable using getter
+        std::cout<<"\nUpdated Author: "<<Book1.getAuthor();
+    }
