d33538530b6c9adce439fa4d9fc2ad4eff8a7b60
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-14 20:22:40 +0500

    docs: added comments

--- week#4/Animal.cpp
+++ week#4/Animal.cpp
@@ -1,18 +1,25 @@
+// Library files
 #include <iostream>
+// uploading cout & string data-type
 using std::cout;
 using std::string;
+// main class
 class Animal {
+
 public:
+    // function
     void run(string name) {
         cout<<"The "<<name<<" is running.";
     }
 };
-
+// child class
 class Dog: public Animal {
 };
 
-
+// main function
 int main() {
+// instance of Dog class
     Dog dog;
-    dog.run("Dog");
+// print of the inherited function.
+    dog.run("Tommy");
 }
