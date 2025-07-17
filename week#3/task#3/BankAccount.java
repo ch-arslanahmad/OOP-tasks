d2bebbdb0256de2b43f3a4cd862a1e4922381f64
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-17 19:18:57 +0500

    feat: added more methods regarding deposit/withdrawal

--- week#3/task#3/BankAccount.java
+++ week#3/task#3/BankAccount.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class BankAccount {
-    static float withdrawal_Limit = 10000;
+    static int withdrawal_Limit = 10000;
     static int deposit_Limit = 10000;
 
     private String name;
@@ -28,6 +28,25 @@ class BankAccount {
         System.out.print("Deposit Limit: " + deposit_Limit);
     }
     
+    void deposit(Scanner input) {
+        System.out.print("Money to Deposit: ");
+        int deposit = input.nextInt();
+        balance = deposit + balance;
+        deposit_Limit = deposit_Limit - deposit;
+    }
+    
+    void withdrawal(Scanner input) {
+        System.out.print("Money to Withdraw: ");
+        int withdrawal = input.nextInt();
+        if(balance<= withdrawal) {
+        balance = balance - withdrawal;
+        withdrawal_Limit = withdrawal_Limit - withdrawal;
+        }
+        else {
+            System.out.println("ERROR! Not Enough Balance");
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         BankAccount Person1 = new BankAccount("Arslan", 100000);
@@ -41,15 +60,17 @@ class BankAccount {
         
             switch(choice) {
                 case 1:
-                    Person1.getBalance();
+                    System.out.println(Person1.getBalance());
                 break;
                 case 2:
                     Person1.getBalance();
                     Person1.getLimit();
                 break;
                 case 3:
+                    Person1.deposit(input);
                 break;
                 case 4:
+                    Person1.withdrawal(input);
                 break;
                 case 5:
                     System.out.println("Goodbye. Have a good day.");
