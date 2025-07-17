7b2d0d8dd909205e4b31fb8bcd7964525d2ac900
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-17 18:32:15 +0500

    feat & bugs: updated the code - however has some errors to fix

--- week#3/task#3/BankAccount.java
+++ week#3/task#3/BankAccount.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class BankAccount {
-    static float withdrawl_Limit = 10000;
+    static float withdrawal_Limit = 10000;
     static int deposit_Limit = 10000;
 
     private String name;
@@ -11,6 +11,11 @@ class BankAccount {
         this.balance = balance;
     }
     
+    int getChoice(Scanner input) {
+        System.out.print("1. See Balance\n2. See Balance with Limits\n3. Deposit Money\n4. Withdraw Money\n\nEnter your choice (1-4): ");
+        int choice =  input.nextInt();
+        return choice;
+    }
 
     String getName() {
         return name;
@@ -18,10 +23,41 @@ class BankAccount {
     int getBalance() {
         return balance;
     }
-    
+    void getLimit() {
+        System.out.print("Withdrawal Limit: " + withdrawal_Limit);
+        System.out.print("Deposit Limit: " + deposit_Limit);
+    }
     
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         BankAccount Person1 = new BankAccount("Arslan", 100000);
-        System.out.println("Your balance is " + Person1.getBalance());
+        
+
+        
+
+        while(true) {
+
+        int choice = Person1.getChoice(input);
+        
+            switch(choice) {
+                case 1:
+                    Person1.getBalance();
+                break;
+                case 2:
+                    Person1.getBalance();
+                    Person1.getLimit();
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+                case 5:
+                    System.out.println("Goodbye. Have a good day.");
+                        input.close();
+                        return;
+                    default:
+                    System.out.println("Enter VALID input...");
+            }
+        }
     }
 }
