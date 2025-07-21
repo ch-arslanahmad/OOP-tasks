be608ffd03c9472fa1e7f339d993c94472da4a4e
Muhammad Arslan <ch.arslanad@gmail.com>
2025-07-21 15:27:58 +0500

    feat: added polymorphism/overriding methods

--- week#4/Animal.java
+++ week#4/Animal.java
@@ -1,3 +1,4 @@
+// This program uses polymorphism and inheritance
 // parent_class
 class Animal {
     boolean alive;
@@ -5,26 +6,35 @@ class Animal {
     void run(String name) {
         System.out.println("The "+ name + " is running.");
     }
+// method of animal: speak
+    void speak() {
+        System.out.println("Generic sounds");
+    }
 
 // main method
     public static void main(String[] args) {
-    // create an instance/object of Dog: dog
-        Dog dog = new Dog();
-    // call run method
+    // create an instance/object of Dog with Animal class reference
+        Animal dog = new Dog();
+    // call run method of Animal Class that Dog inherited.
     dog.run("Dog");
-    // call speak method
+    // call overriden speak method
     dog.speak();
+    // Use super in method: had to use downcasting, which essentially temporarily makes you use the Dog class specific method in polymorphism. 
+    ((Dog) dog).superWork();
     }    
 }
 // child_class
 class Dog extends Animal{
-    // method of dog: speak
-    void speak() {
+    // method of dog: speak (overriding method)
+    @Override void speak() {
         // use of super in method;
+        System.out.println("Dog says: Woof Woof!");
+    }
+    
+    void superWork() {
         super.run("Super");
         // use of super in class;
         super.alive = true;
         System.out.println("Alive: " + super.alive);
-        System.out.println("Dog says: Woof Woof!");
     }
 }
