4cd4f8f16f5427854b8ebea3a0bbf278117d396f
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-24 15:09:11 +0500

    feat: added Rectangle class

--- week#4/task#2/Shapes.cpp
+++ week#4/task#2/Shapes.cpp
@@ -10,6 +10,8 @@ class Shapes {
 public:
 // members/attributes
     float radius;
+    float width;
+    float height;
 //  UPDATED: declaring & initilizing static variable in class: only possible using constexpr
     constexpr static float PI = 3.14;
 };
@@ -24,13 +26,29 @@ public:
 // findArea method
     void findArea() {
         float area = (radius * radius) * PI;
-        cout<<"The area of Circle: "<<area;
+        cout<<"The area of Circle: "<<area<<"\n";
+    }
+};
+
+class Rectangle : public Shapes {
+public:
+
+    Rectangle(float width, float height) {
+        this->width = width;
+        this->height = height;
+    }
+
+    void findArea() {
+        float area = width * height;
+        cout<<"The area of Rectangle: "<<area;
     }
 };
 
 int main() {
     // creating the object
     Circle circle(10);
+    Rectangle rectangle(10, 10);
     // calling the method of the object
     circle.findArea();
+    rectangle.findArea();
 }
