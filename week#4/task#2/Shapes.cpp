4b687e037640612b691b94d855668fdef39ccd40
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-24 14:57:16 +0500

    feat: fixed errors and added comments

--- week#4/task#2/Shapes.cpp
+++ week#4/task#2/Shapes.cpp
@@ -1,29 +1,39 @@
+// NOTE: OUTPUT IS ZERO: fix that
+
+// header file
 #include<iostream>
+// using these for easy string & output
 using std::string;
 using std::cout;
 
+// main/parent class
 class Shapes {
+// public modifier: everything is private by default in C++
 public:
+// members/attributes
     float radius;
+// only declaring static as it cannot be initilized in class
     static float PI;
-
-    float height;
-    float width;
-
-
 };
+// initilized the static variables
+float Shapes::PI = 3.14;
 
-class Circle : Shapes {
-    Circle(float radius): radius(radius) {}
-
-    virtual void findArea() {
+// child class
+class Circle : public Shapes {
+public:
+// Circle constructor using attributes from parent
+    Circle(float radius) {
+    }
+// findArea method
+    void findArea() {
         float area = (radius * radius) * PI;
         cout<<"The area of Circle: "<<area;
     }
 };
 
 int main() {
-    Shapes::PI = 3.14f;
-    Shapes circle(10);
+    // creating the object
+    Circle circle(10);
+    // calling the method of the object
     circle.findArea();
 }
