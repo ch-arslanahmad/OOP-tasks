51b0a2f4b5a1191a1eed8a44ff76dcdc94fc1ae8
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-25 15:27:16 +0500

    feat: Implement method overriding example

--- week#5/Vehicle.java
+++ week#5/Vehicle.java
@@ -1,27 +1,48 @@
-// This is a simple example of base class polymorphism, can also be called runtime polymorphism in which child method is the selected/overidded method during runtime.
+// Implement method overriding examples
+// finding speed of a specific model using polymorphism
 // public class
 public class Vehicle {
-
+    int speed;
     // parent method
-    void run() {
-        System.out.print("Vehicle is running");
+    void showSpeed() {
+        System.out.print("Speed of Vehicle depends on type & company.");
     }
 
     // main methid
     public static void main(String[] args) {
     // creating object of car in Vehicle class
-        Vehicle car = new Car();
+        Vehicle car = new Car(100);
+    // creating object of Honda in Car class
+        Car honda = new Honda(70);
     // calling run overriden method
-        car.run();
+        car.showSpeed();
+        honda.showSpeed();
     }
 
 }
 // child class
 class Car extends Vehicle {
 
+    Car(int speed){
+        this.speed = speed;
+    }
+
     // overriden method
     @Override
-    void run() {
-        System.out.println("Car is running.");
+    void showSpeed() {
+        System.out.println("Car max speed is around " + speed + "kmh.");
+    }
+}
+
+class Honda extends Car {
+
+    Honda(int speed) {
+        super(speed);
+    }
+
+    // overriding the overridden method
+    @Override
+    void showSpeed() {
+        System.out.println("Honda max speed is around " + speed + "kmh.");
     }
 }
