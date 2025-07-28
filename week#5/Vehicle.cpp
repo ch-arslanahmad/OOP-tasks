f49696820730ea2ce4e3db6a88d679a7549a3c15
ch-arslanahmad <ch.arslanad@gmail.com>
2025-07-28 16:30:20 +0500

    style: removed unecessary virtual keyword

--- week#5/Vehicle.cpp
+++ week#5/Vehicle.cpp
@@ -1,5 +1,7 @@
 // Implement method overriding examples
 // finding speed of a specific model using polymorphism
+// Dynamic dispatch in C++ is done by vTables, This is achieved by creating a reference of object in a base class pointing to the child class. Enabling overriding via C++ by virtual methods (using virtual keyword), that allows it to execute method not based on reference type rather where the object is i.e., child class, this is done during runtime, This is called runtime polymorphism.
+
 #include<iostream>
 using std::string;
 using std::cout;
@@ -20,7 +22,7 @@ public:
         this->speed = speed;
     }
 
-    virtual void showSpeed() {
+     void showSpeed() override{
         cout<<"Car max speed is around "<<speed<<"kmh.\n";
     }
 
@@ -29,7 +31,7 @@ public:
 class Honda : public Car {
 public:
     Honda(int speed) : Car(speed) {}
-    virtual void showSpeed() {
+    void showSpeed() override {
         cout<<"Honda max speed is around "<<speed<<"kmh.";
     }    
 
