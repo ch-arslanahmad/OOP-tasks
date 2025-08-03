bfddf88e5f6388823a573a8eac44f2aee6c2bce5
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-03 08:27:48 +0500

    feat(logAuth): added limit to sign-ins

--- week#6/task#3/logAuth.java
+++ week#6/task#3/logAuth.java
@@ -7,38 +7,55 @@ public class logAuth {
     private static final Logger logger = Logger.getLogger(logAuth.class.getName());
 
     void getInput(Scanner input) {
-        System.out.print("Enter name:");
+        System.out.print("Enter name: ");
         name = input.nextLine();
-        System.out.print("Enter Password:");
+
+        System.out.print("\nEnter Password: ");
         pass = input.nextLine();
     }
 
-    void doAuth() {
+    boolean doAuth(int n) {
+
+        boolean isCorrect = false;
         try {
         FileHandler fh = new FileHandler("OOP-tasks/week#6/task#3/log.txt", true);
         fh.setFormatter(new SimpleFormatter());
         logger.addHandler(fh);
-            if(!pass.equals(PASSWORD)) {
-                logger.setLevel(Level.FINE);
-                logger.info("Entered Incorrect Password.");
+        logger.setLevel(Level.FINE);
+        if(!pass.equals(PASSWORD)) {
+                logger.info("Try " + n + ": Entered Incorrect Password.");
                 logger.severe("Illegal Authorization");
             }
             else {
                 System.out.println("Welcome back, " + name);
+                isCorrect = true;
+                logger.info("User logged in");
             }
         } catch (Exception e) {
             System.out.println("Error Occured");
             e.printStackTrace();
         }
 
+        return isCorrect;
     }
 
 
     public static void main(String[] args) {
         logAuth file = new logAuth();
         Scanner input = new Scanner(System.in);
+
+        int n = 1;
+        while(n <=3) { 
         file.getInput(input);
-        file.doAuth();
+        boolean a = file.doAuth(n);
+        n++;
+            if(a) {
+                break;
+            }
+            else {
+                System.out.println("\nTry Again");
+            }
+        }
     }
     
 }
