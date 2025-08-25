3c3298e6aa8c530278567586372104657798c073
ch-arslanahmad <ch.arslanad@gmail.com>
2025-08-25 19:28:37 +0500

    feat(Array): 1. fixed the reverse array 2. added arraysSum(), sumArray)(), max & minArray()

--- week#10/Array.java
+++ week#10/Array.java
@@ -26,26 +26,70 @@ class Array {
         return min;
     }
 
-    // FIX THIS REVERSE METHOD ARRAY
     int[] reverseArray(int[] array) {
         int size = array.length;
         int[] reverse = new int[size];
-        // for example: size = 5;
-        /* so i = 4, 4>= 0; 4-- = 3 */
-        for (int i = (size - 1); i >= 0; i--) {
+        int j = 0;
+        for (int i = (array.length - 1); i >= 0; i--) {
             // reverse []
-            reverse[size - i] = array[i];
+            reverse[j] = array[i];
+            j++;
         }
         return reverse;
+    }
+
+    // sum of 2 arrays
+    int arraysSum(int[] array1, int[] array2) {
+        int sum1 = sumArray(array1);
+        int sum2 = sumArray(array2);
+        return sum1 + sum2;
+    }
+
+    // sum of an array
+    int sumArray(int[] array) {
+        int sum = 0;
+        for (int s : array) {
+            sum += s;
+        }
+        return sum;
+    }
+
+    // max value in an array
+    int maxArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return 1;
+    }
+
+    // min value in an array
+    int minArray(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
         Array array = new Array();
         int[] arr = { 5, -100, 10, 1000, 5 };
+        int[] arr2 = { 1, 2, 3, 4 };
         System.out.println(" Largest value in array: " + array.findLargest(arr));
         System.out.println(" Smallest value in array: " + array.findSmallest(arr));
 
         System.out.println("Reversed Array: ");
-        Arrays.stream(array.reverseArray(arr)).forEach(System.out::println);
+        int[] reverseArr = array.reverseArray(arr);
+        for (int a : reverseArr) {
+            System.out.print(a + "\t");
+        }
+        System.out.println("\n");
+
+        System.out.println(array.arraysSum(arr, arr2));
     }
 }
